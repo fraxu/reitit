a035da58a09af54d2debcd27437e778d0fb3900a
Antti Leppänen <antti.leppanen@solita.fi>
2019-04-01 14:44:34 +0300

    Revert "Change version"
    This reverts commit 5161a96c271eb21032d0c52cdc1443ca083b677e.

--- modules/reitit-middleware/project.clj
+++ modules/reitit-middleware/project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/reitit-middleware "shadow-dom-fix-v3"
+(defproject metosin/reitit-middleware "0.3.1"
   :description "Reitit, common middleware bundled"
   :url "https://github.com/metosin/reitit"
   :license {:name "Eclipse Public License"
